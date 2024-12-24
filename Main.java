911c6627f33fa795b4cf316140a98bcd87694821
Masouma-kanso <masoumakanso@gmail.com>
2024-12-24 09:00:42 -0800

    The main class that runs the program and creates an instance of the Notebook class

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+/**
+ * The Main Class to Run.
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        new Notebook().mainMenu();
+    }
+}
